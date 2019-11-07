ca4af13cc579c353c0374237cb9250d5f4a5b717
Bryson Lingenfelter <blingenfelter@nevada.unr.edu>
2019-11-07 10:35:15 -0800

    fixed plotting code

--- 3_programming_assignment/project3.py
+++ 3_programming_assignment/project3.py
@@ -10,12 +10,21 @@ experiment3 = True
 
 # Experiment 1
 # Part 1
-# TODO: center transform
-def plot_transform_1d(data):
-    mag = [(data[i]**2 + data[i+1]**2)**(1/2) for i in range(len(data)//2)]
-    plt.plot([data[i] for i in range(len(data)//2)], 'o-', label="real")
-    plt.plot([data[i+1] for i in range(len(data)//2)], 'o-', label="imaginary")
-    plt.plot(mag, 'o-', label="magnitude")
+def center_transform(data):
+    new_data = []
+    for i in range(len(data)):
+        new_data.append(data[i]*(-1)**(i))
+    return new_data
+
+def plot_transform_1d(data, centered=True):
+    if centered:
+        x = range(-len(data)//4, len(data)//4)
+    else:
+        x = range(len(data)//2)
+    mag = [(data[2*i]**2 + data[2*i+1]**2)**(1/2) for i in range(len(data)//2)]
+    plt.plot(x, [data[2*i] for i in range(len(data)//2)], label="real")
+    plt.plot(x, [data[2*i+1] for i in range(len(data)//2)], label="imaginary")
+    plt.plot(x, mag, label="magnitude")
     plt.axhline(0, lw=1, color="grey")
     plt.legend()
     plt.show()
@@ -26,7 +35,7 @@ def gen_cos_wave(u, N):
 if experiment1:
     data = [2, 3, 4, 4]
     transform = fft_func.fourier_transform(data, 1)
-    plot_transform_1d(transform)
+    plot_transform_1d(transform, centered=False)
     print(transform)
 
     inverse_transform = fft_func.fourier_transform(transform, -1)
@@ -36,7 +45,7 @@ if experiment1:
     cos_wave = gen_cos_wave(8, 128)
     plt.plot(cos_wave)
     plt.show()
-    cos_transform = fft_func.fourier_transform(cos_wave, 1)
+    cos_transform = fft_func.fourier_transform(center_transform(cos_wave), 1)
     plot_transform_1d(cos_transform)
 
     # Part 3
@@ -47,7 +56,7 @@ if experiment1:
     plt.plot(rect_func)
     plt.show()
 
-    rect_transform = fft_func.fourier_transform(rect_func, 1)
+    rect_transform = fft_func.fourier_transform(center_transform(rect_func), 1)
     plot_transform_1d(rect_transform)
 
 # Experiment 2
