2586321d1bc4f4030a26201f9292d798b56a03da
Nate Thom <39501007+NateThom@users.noreply.github.com>
2019-11-19 12:02:11 -0800

    Nate pa3 (#1)
    * normalize, center2d, plot2d (why?), and 2ddft
    
    * Move code to remote for access.
    
    * Added comments to 2ddft
    
    * Working FT, centering, un-centering, and magnitude plotting.
    
    * Fix fft.py
    
    * Forier transform is working and second half of experiment 3
    
    * Current version of code
    
    * working? frequency filtering

--- 3_programming_assignment/project3.py
+++ 3_programming_assignment/project3.py
@@ -1,11 +1,13 @@
 import fft
+from PIL import Image
 import matplotlib.pyplot as plt
-from math import cos, pi
+from math import sin, cos, pi, atan2, log, sqrt
+import numpy as np
 
 fft_func = fft.FFT()
 
-experiment1 = True
-experiment2 = True
+experiment1 = False
+experiment2 = False
 experiment3 = True
 
 # Experiment 1
@@ -59,6 +61,214 @@ if experiment1:
     rect_transform = fft_func.fourier_transform(center_transform(rect_func), 1)
     plot_transform_1d(rect_transform)
 
-# Experiment 2
+
+def add_zero(data):
+    new_data = []
+    for x in range(len(data)):
+        new_data.append([])
+        for y in range(len(data[x])):
+            new_data[x].append(data[x][y])
+            new_data[x].append(0)
+    return new_data
+
+def dft2d(data, mode):
+    n = len(data[0])
+    if mode is 1:
+        data = add_zero(data)
+
+    transform = []
+    # dft of rows
+    for i in range(0, len(data)):
+        transform.append(fft_func.fourier_transform(data[i], mode, extend=False))
+
+    # multiply by n
+    if mode is 1:
+        for i in range(len(transform)):
+            for j in range(len(transform[i])):
+                transform[i][j] *= n
+    # get columns
+    columns = []
+    for i in range(len(transform[0]) // 2):
+        columns.append([])
+        for row in transform:
+            columns[i].append(row[i*2])
+            columns[i].append(row[i*2+1])
+    # transform columns
+    for x in range(0, len(columns)):
+        columns[x] = fft_func.fourier_transform(columns[x], mode, extend=False)
+    # convert columns to rows
+    # get rows
+    transform = []
+    for i in range(len(columns[0]) // 2):
+        transform.append([])
+        for row in columns:
+            transform[i].append(row[i * 2])
+            transform[i].append(row[i * 2 + 1])
+
+    return transform
+
+def normalize_0_255(input_list):
+    min = 999999
+    max = -999999
+    for width in range(len(input_list)):
+        for height in range(len(input_list[width])):
+            if min > input_list[width][height]:
+                min = input_list[width][height]
+            if max < input_list[width][height]:
+                max = input_list[width][height]
+
+    for width in range(len(input_list)):
+        for height in range(len(input_list[width])):
+            input_list[width][height] = int((input_list[width][height] - min) * (255 / (max - min)))
+
+    return input_list
+
+def center_2d_transform(data):
+    new_data = []
+    for i in range(len(data)):
+        new_data.append([])
+        for j in range(len(data[i])):
+            new_data[i].append(data[i][j]*(-1)**(i+j))
+    return new_data
+
+def remove_centering_2d_transform(data):
+    data = remove_zeros(data)
+    new_data = []
+    for i in range(len(data)):
+        new_data.append([])
+        for j in range(len(data[i])):
+            new_data[i].append(data[i][j] * (-1) ** (i + j))
+    return new_data
+
+def remove_zeros(data):
+    new_data = []
+    for i in range(len(data)):
+        new_data.append([])
+        for j in range(0, len(data[i]), 2):
+            new_data[i].append(data[i][j])
+    return new_data
+
+def plot_transform_2d(data, centered=True):
+    if centered:
+        x = range(-(len(data) * 2) // 4, (len(data) * 2) // 4)
+        y = range(-len(data[0]) // 4, len(data[0]) // 4)
+    else:
+        x = range((len(data) * 2) // 2)
+        y = range(len(data[0]) // 2)
+
+    # magnitude
+    mag = []
+    for i in range(len(data)):
+        mag.append([])
+        for j in range(len(data[i]) // 2):
+            mag[i].append(log(((data[i][2 * j] ** 2) + (data[i][2 * j + 1] ** 2)) ** (1 / 2) + 1))
+        print()
+
+    plt.imshow(mag, label="magnitude", cmap=plt.get_cmap("gray"))
+    plt.show()
+
+def SquareImage(squareSize):
+    size = squareSize/2
+    size = int(size)
+    im = Image.new("L", (512, 512), "black")
+    im.paste("white", (128-size,128-size, 128+size,128+size))
+    im.paste("white", (256-size, 256-size, 256+size, 256+size))
+    return im
+
+if experiment2:
+    #convert from square image
+    squareImage = SquareImage(128)
+    numpyArray = np.array(squareImage)
+    #to numpy array
+    squareList = numpyArray.tolist()
+
+    # run 2dFFT where 1 is forward and -1 is inverse FFTs
+    plt.imshow(squareList, cmap=plt.get_cmap("gray"))
+    plt.show()
+    transform = dft2d(center_2d_transform(squareList), 1)
+
+    plot_transform_2d(transform)
+    # Set phase equal to zero
+    for i in range(len(transform)):
+        for j in range(0, len(transform[i]), 2):
+            transform[i][j] = sqrt((transform[i][j] ** 2) + (transform[i][j + 1] ** 2))
+            transform[i][j + 1] = 0
+
+    inverseTransform = dft2d(transform, -1)
+    inverseTransform = remove_centering_2d_transform(inverseTransform)
+    plt.imshow(inverseTransform, cmap=plt.get_cmap("gray"))
+    plt.show()
+    print()
 
 # Experiment 3
+
+if experiment3:
+    lenna = Image.open("../images-pgm/lenna.pgm")
+    width, height = lenna.size
+    lenna_pixels = lenna.load()
+
+    lenna_list = []
+    for x in range(width):
+        lenna_list.append([])
+        for y in range(height):
+            lenna_list[x].append(lenna.getpixel((y,x)))
+
+    # test
+    plt.imshow(lenna_list, cmap=plt.get_cmap("gray"))
+    plt.show()
+    lenna_transform = dft2d(center_2d_transform(lenna_list), 1)
+    plot_transform_2d(lenna_transform)
+    lenna_inverse_transform = dft2d(lenna_transform, -1)
+    lenna_inverse_transform = remove_centering_2d_transform(lenna_inverse_transform)
+    plt.imshow(lenna_inverse_transform, cmap=plt.get_cmap("gray"))
+    plt.show()
+
+
+    # part 3a
+    lenna_transform = dft2d(center_2d_transform(lenna_list), 1)
+
+
+    plot_transform_2d(lenna_transform)
+
+    # Set phase equal to zero
+    for i in range(len(lenna_transform)):
+        for j in range(0, len(lenna_transform[i]), 2):
+            lenna_transform[i][j] = sqrt((lenna_transform[i][j] ** 2) + (lenna_transform[i][j + 1] ** 2))
+            lenna_transform[i][j+1] = 0
+
+    lenna_transform = dft2d(lenna_transform, -1)
+    lenna_transform = remove_centering_2d_transform(lenna_transform)
+    # lenna_inverse_transform = remove_zeros(lenna_inverse_transform)
+    plt.imshow(lenna_transform, cmap=plt.get_cmap("gray"))
+    plt.show()
+
+    # part 3b
+    lenna_transform = dft2d(center_2d_transform(lenna_list), 1)
+    plot_transform_2d(lenna_transform)
+
+    # Set phase to original and magnitude to 1
+    for i in range(len(lenna_transform)):
+        for j in range(0, len(lenna_transform[i]), 2):
+            theta = (atan2(lenna_transform[i][j+1], lenna_transform[i][j]))
+            lenna_transform[i][j] = cos(theta)
+            lenna_transform[i][j+1] = sin(theta)
+
+    plot_transform_2d(lenna_transform)
+
+    lenna_inverse_transform = dft2d(lenna_transform, -1)
+    lenna_inverse_transform = remove_centering_2d_transform(lenna_inverse_transform)
+    lenna_inverse_transform = normalize_0_255(lenna_inverse_transform)
+    plt.imshow(lenna_inverse_transform, cmap=plt.get_cmap("gray"))
+    plt.show()
+    print()
+
+# data = [[1, 2, 3, 4, 5],[249, 250, 251, 253, 254],[1, 2, 3, 4, 5],[249, 250, 251, 253, 254],[50,50,50,50,50]]
+# plt.imshow(data, cmap=plt.get_cmap("gray"))
+# plt.show()
+# transform = dft2d(center_2d_transform(data), 1)
+# plot_transform_2d(transform, centered=True)
+# inverse_transform = dft2d(transform, -1)
+# inverse_transform = remove_centering_2d_transform(inverse_transform)
+# plt.imshow(inverse_transform, cmap=plt.get_cmap("gray"))
+# plt.show()
+# print()
---
+++ 4/restoration.py
@@ -0,0 +1,189 @@
+from PIL import Image
+import fft
+import matplotlib.pyplot as plt
+from math import cos, sin, pi, e, log
+import numpy as np
+
+def add_zero(data):
+    new_data = []
+    for x in range(len(data)):
+        new_data.append([])
+        for y in range(len(data[x])):
+            new_data[x].append(data[x][y])
+            new_data[x].append(0)
+    return new_data
+
+def remove_zeros(data):
+    new_data = []
+    for i in range(len(data)):
+        new_data.append([])
+        for j in range(0, len(data[i]), 2):
+            new_data[i].append(data[i][j])
+    return new_data
+
+def plot_transform_2d(data, use_log=True):
+    # magnitude
+    mag = []
+    for i in range(len(data)):
+        mag.append([])
+        for j in range(len(data[i]) // 2):
+            if use_log:
+                mag[i].append(log(((data[i][2*j]**2) + (data[i][2*j + 1]**2))**(1/2) + 1))
+            else:
+                mag[i].append(((data[i][2*j]**2) + (data[i][2*j + 1]**2))**(1/2))
+
+    plt.imshow(mag, label="magnitude", cmap=plt.get_cmap("gray"))
+    plt.show()
+
+def dft2d(data, mode):
+    n = len(data)
+    if mode is 1:
+        data = add_zero(data)
+
+    transform = []
+    # dft of rows
+    for i in range(len(data)):
+        transform.append(fft_func.fourier_transform(data[i], mode, extend=False))
+
+    # multiply by n
+    if mode is 1:
+        for i in range(len(transform)):
+            for j in range(len(transform[i])):
+                transform[i][j] *= n
+
+    # get columns
+    columns = []
+    for x in range(len(transform)):
+        columns.append([])
+        for y in range(len(transform)):
+            columns[x].append(transform[y][x*2])
+            columns[x].append(transform[y][x*2+1])
+
+    # transform columns
+    for x in range(0, len(columns)):
+        columns[x] = fft_func.fourier_transform(columns[x], mode, extend=False)
+    # convert columns to rows
+    transform = []
+    for x in range(len(columns)):
+        transform.append([])
+        for y in range(len(columns)):
+            transform[x].append(columns[y][x*2])
+            transform[x].append(columns[y][x*2+1])
+
+    return transform
+
+def center_2d_transform(data):
+    new_data = []
+    for i in range(len(data)):
+        new_data.append([])
+        for j in range(len(data[i])):
+            new_data[i].append(data[i][j]*(-1)**(i+j))
+    return new_data
+
+def remove_centering_2d_transform(data):
+    data = remove_zeros(data)
+    return center_2d_transform(data)
+
+def motion_blur():
+    pass
+
+fft_func = fft.FFT()
+
+def matmul(a, b):
+    result = []
+    for i in range(len(a)):
+        result.append([])
+        for j in range(len(b[0])):
+            sum = 0
+            for k in range(len(b)):
+                sum += a[i][k]*b[k][j]
+            result[-1].append(sum)
+
+    return result
+
+def complex_hadamard(a, b):
+    result = []
+    for i in range(len(a)):
+        result.append([])
+        for j in range(len(a[0])//2):
+            real_sum = a[i][2*j]*b[i][2*j] - a[i][2*j+1]*b[i][2*j+1]
+            complex_sum = a[i][2*j]*b[i][2*j+1] + a[i][2*j+1]*b[i][2*j]
+            result[-1].append(real_sum)
+            result[-1].append(complex_sum)
+
+    return result
+
+def pad_zeros(image, filter):
+    n = len(image)
+    m = len(filter)
+
+    # Padding must be at least n+m-1
+    min_padding = n+m-1
+    # Pad to a power of 2
+    padding = 0
+    exponent = 1
+    while padding < min_padding:
+        padding = 2**exponent
+        exponent+=1
+
+    for i in range(m,padding):
+        filter.append([0]*(padding))
+    for i in range(m):
+        filter[i].extend([0]*(padding-m))
+
+    for i in range(n,padding):
+        image.append([0]*(padding))
+    for i in range(n):
+        image[i].extend([0]*(padding-n))
+
+    return image, filter, padding - n
+
+def unpad(image, pad_len):
+    for i in range(pad_len):
+        image[i] = image[i][:pad_len*2]
+
+    return image[:pad_len]
+
+a = [[1, 1, 3, 0],
+     [2, 0, 1, 1]]
+
+b = [[2,  0, 1, 0],
+     [2, -1, 3, 3]]
+
+print(complex_hadamard(a, b))
+
+image = Image.open("nate.pgm")
+
+width, height = image.size
+image_pixels = image.load()
+image_list = []
+for x in range(width):
+    image_list.append([])
+    for y in range(height):
+        image_list[x].append(image.getpixel((y,x)))
+
+def gaussian_filter(size, var):
+    return [[gauss(i - size//2, j - size//2,var) for i in range(size)]
+                                                  for j in range(size)]
+
+def gauss(x, y, var):
+    return 1/(2*pi*var)*e**(-(x**2+y**2)/(2*var))
+
+filter = gaussian_filter(7, 1.6)
+
+plot_transform_2d(add_zero(image_list), use_log=False)
+image_list, filter, pad_len = pad_zeros(image_list, filter)
+
+image_transform = dft2d(center_2d_transform(image_list), 1)
+filter_transform = dft2d(center_2d_transform(filter), 1)
+plot_transform_2d(image_transform)
+plot_transform_2d(filter_transform)
+
+filtered_image_freq = complex_hadamard(image_transform, filter_transform)
+
+image_inverse_transform = dft2d(filtered_image_freq, -1)
+remove_centering_2d_transform(image_inverse_transform)
+
+filtered_image = unpad(image_inverse_transform, pad_len)
+
+plot_transform_2d(filtered_image, use_log=False)
