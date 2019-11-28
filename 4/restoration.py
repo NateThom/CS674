d90f8ca7a62b1949cdde3c1ef6277aee5f59c762
Bryson Lingenfelter <blingenfelter@nevada.unr.edu>
2019-11-27 17:39:32 -0800

    motion blur restoration

--- 4/restoration.py
+++ 4/restoration.py
@@ -21,16 +21,22 @@ def remove_zeros(data):
             new_data[i].append(data[i][j])
     return new_data
 
-def plot_transform_2d(data, use_log=True):
-    # magnitude
+def get_magnitude(data, use_log=True, square=False):
     mag = []
     for i in range(len(data)):
         mag.append([])
         for j in range(len(data[i]) // 2):
-            if use_log:
+            if square:
+                mag[i].append((data[i][2*j]**2) + (data[i][2*j + 1]**2))
+            elif use_log:
                 mag[i].append(log(((data[i][2*j]**2) + (data[i][2*j + 1]**2))**(1/2) + 1))
             else:
                 mag[i].append(((data[i][2*j]**2) + (data[i][2*j + 1]**2))**(1/2))
+
+    return mag
+
+def plot_transform_2d(data, use_log=True):
+    mag = get_magnitude(data, use_log)
 
     plt.imshow(mag, label="magnitude", cmap=plt.get_cmap("gray"))
     plt.show()
@@ -84,9 +90,6 @@ def remove_centering_2d_transform(data):
     data = remove_zeros(data)
     return center_2d_transform(data)
 
-def motion_blur():
-    pass
-
 fft_func = fft.FFT()
 
 def matmul(a, b):
@@ -107,61 +110,103 @@ def complex_hadamard(a, b):
     for i in range(len(a)):
         result.append([])
         for j in range(len(a[0])//2):
-            real_sum = a[i][2*j]*b[i][2*j] - a[i][2*j+1]*b[i][2*j+1]
-            complex_sum = a[i][2*j]*b[i][2*j+1] + a[i][2*j+1]*b[i][2*j]
-            result[-1].append(real_sum)
-            result[-1].append(complex_sum)
+            real_part = a[i][2*j]*b[i][2*j] - a[i][2*j+1]*b[i][2*j+1]
+            complex_part = a[i][2*j]*b[i][2*j+1] + a[i][2*j+1]*b[i][2*j]
+            result[-1].append(real_part)
+            result[-1].append(complex_part)
 
     return result
 
-def pad_zeros(image, filter):
+def complex_division(a, b, cutoff=30):
+    result = []
+    for i in range(len(a)):
+        result.append([])
+        for j in range(len(a[0])//2):
+            if ((j-len(a)//2)**2 + (i-len(a)//2)**2)**(1/2) > cutoff:
+                result[-1].append(a[i][2*j])
+                result[-1].append(a[i][2*j+1])
+                continue
+            real_part = (a[i][2*j]*b[i][2*j] + a[i][2*j+1]*b[i][2*j+1]) \
+                        / (b[i][2*j]**2 + b[i][2*j+1]**2)
+            complex_part = (a[i][2*j+1]*b[i][2*j] - a[i][2*j]*b[i][2*j+1]) \
+                           / (b[i][2*j]**2 + b[i][2*j+1]**2)
+            result[-1].append(real_part)
+            result[-1].append(complex_part)
+
+    return result
+
+def complex_invert(matrix, cutoff=30):
+    result = []
+    for i in range(len(matrix)):
+        result.append([])
+        for j in range(len(matrix[0])//2):
+            if ((j-len(matrix)//2)**2 + (i-len(matrix)//2)**2)**(1/2) > cutoff:
+                result[-1].append(1)
+                result[-1].append(1)
+                continue
+            real_part = matrix[i][2*j] / (matrix[i][2*j]**2 + matrix[i][2*j+1]**2)
+            complex_part = -matrix[i][2*j+1] / (matrix[i][2*j]**2 + matrix[i][2*j+1]**2)
+            result[-1].append(real_part)
+            result[-1].append(complex_part)
+
+    return result
+
+def complex_square(matrix):
+    result = []
+    for i in range(len(matrix)):
+        result.append([])
+        for j in range(len(matrix[0])//2):
+            real_part = matrix[i][2*j]**2 - matrix[i][2*j+1]**2
+            complex_part = 2*matrix[i][2*j]*matrix[i][2*j+1]
+            result[-1].append(real_part)
+            result[-1].append(complex_part)
+
+    return result
+
+def add_scalar(matrix, scalar):
+    result = []
+    for i in range(len(matrix)):
+        result.append([])
+        for j in range(len(matrix[0])//2):
+            real_part = matrix[i][2*j] + scalar
+            complex_part = matrix[i][2*j+1]
+            result[-1].append(real_part)
+            result[-1].append(complex_part)
+
+    return result
+
+def pad_zeros(image, filter, pad_value=0):
     n = len(image)
     m = len(filter)
 
-    # Padding must be at least n+m-1
-    min_padding = n+m-1
+    # Padding must be at least n + m - 1
+    min_padding = n + m -1
     # Pad to a power of 2
     padding = 0
     exponent = 1
     while padding < min_padding:
         padding = 2**exponent
-        exponent+=1
+        exponent += 1
 
     for i in range(m,padding):
-        filter.append([0]*(padding))
+        filter.append([0 for _ in range(padding)])
     for i in range(m):
-        filter[i].extend([0]*(padding-m))
+        filter[i].extend([0 for _ in range(padding-m)])
 
     for i in range(n,padding):
-        image.append([0]*(padding))
+        image.append([pad_value for _ in range(padding)])
     for i in range(n):
-        image[i].extend([0]*(padding-n))
+        image[i].extend([pad_value for _ in range(padding-n)])
 
     return image, filter, padding - n
 
 def unpad(image, pad_len):
+    if pad_len == 0:
+        return image
     for i in range(pad_len):
-        image[i] = image[i][:pad_len*2]
+        image[i] = image[i][:pad_len]
 
     return image[:pad_len]
-
-a = [[1, 1, 3, 0],
-     [2, 0, 1, 1]]
-
-b = [[2,  0, 1, 0],
-     [2, -1, 3, 3]]
-
-print(complex_hadamard(a, b))
-
-image = Image.open("nate.pgm")
-
-width, height = image.size
-image_pixels = image.load()
-image_list = []
-for x in range(width):
-    image_list.append([])
-    for y in range(height):
-        image_list[x].append(image.getpixel((y,x)))
 
 def gaussian_filter(size, var):
     return [[gauss(i - size//2, j - size//2,var) for i in range(size)]
@@ -170,21 +215,176 @@ def gaussian_filter(size, var):
 def gauss(x, y, var):
     return 1/(2*pi*var)*e**(-(x**2+y**2)/(2*var))
 
-filter = gaussian_filter(7, 1.6)
+def normalize_0_255(input_list):
+    min_v = min([min(i) for i in input_list])
+    max_v = max([max(i) for i in input_list])
 
-plot_transform_2d(add_zero(image_list), use_log=False)
-image_list, filter, pad_len = pad_zeros(image_list, filter)
+    for w in range(len(input_list)):
+        for h in range(len(input_list[w])):
+            input_list[w][h] = int((input_list[w][h] - min_v)
+                                   * (255 / (max_v - min_v)))
 
-image_transform = dft2d(center_2d_transform(image_list), 1)
-filter_transform = dft2d(center_2d_transform(filter), 1)
-plot_transform_2d(image_transform)
-plot_transform_2d(filter_transform)
+    return input_list
 
-filtered_image_freq = complex_hadamard(image_transform, filter_transform)
+def list_to_image(image_list):
+    height = len(image_list)
+    width = len(image_list[0])
+    output_image = Image.new("L", (width, height))
+    output_image_pixels = output_image.load()
+    for i in range(height):
+        for j in range(width):
+            output_image_pixels[j,i] = int(image_list[i][j])
+    return output_image
 
-image_inverse_transform = dft2d(filtered_image_freq, -1)
-remove_centering_2d_transform(image_inverse_transform)
+def get_image_list(path):
+    image = Image.open(path)
 
-filtered_image = unpad(image_inverse_transform, pad_len)
+    width, height = image.size
+    image_pixels = image.load()
+    image_list = []
+    for x in range(width):
+        image_list.append([])
+        for y in range(height):
+            image_list[x].append(image.getpixel((y,x)))
 
-plot_transform_2d(filtered_image, use_log=False)
+    return image_list
+
+def motion_blur_filter(T, a, b, size):
+    p1 = lambda u,v: T/(pi*(u*a + v*b + 0.00001))
+    real_p = lambda u,v: p1(u,v)*1/2*sin(2*pi*(u*a + v*b + 0.00001))
+    imaginary_p = lambda u,v: -p1(u,v)*sin(pi*(u*a + v*b + 0.00001))**2
+
+    filter = []
+    for i in range(-size//2+1, size//2+1):
+        filter.append([])
+        for j in range(-size//2+1, size//2+1):
+            real_part = real_p(i,j)
+            im_part = imaginary_p(i,j)
+            #filter[-1].append(real_part)
+            #filter[-1].append(im_part)
+            if real_part > 0:
+                filter[-1].append(max(real_part, .05))
+            else:
+                filter[-1].append(min(real_part, -.05))
+            if im_part > 0:
+                filter[-1].append(max(im_part, .05))
+            else:
+                filter[-1].append(min(im_part, -.05))
+
+    return filter
+
+def normalize_2d(input_list):
+    list_sum = 0
+    for i in input_list:
+        list_sum += sum(i)
+
+    output_list = [[float(input_list[i][j])/list_sum
+                    for j in range(len(input_list[i]))]
+                   for i in range(len(input_list))]
+
+    return output_list
+
+def add_noise(matrix, mu, sigma):
+    result = []
+    for i in range(len(matrix)):
+        result.append([])
+        for j in range(len(matrix[0])):
+            result[-1].append(max(min(int(matrix[i][j]
+                                      + np.random.normal(mu,sigma)), 255),0))
+
+    return result
+
+edges_test = False
+apply_motion_blur = True
+remove_motion_blur = True
+remove_motion_blur_wiener = True
+
+if edges_test:
+    filter = [[1,1,1],[0,0,0],[-1,-1,-1]]
+    nate = get_image_list("nate.pgm")
+
+    nate, filter, pad_len = pad_zeros(nate, filter)
+    centered_nate = center_2d_transform(nate)
+    centered_filter = center_2d_transform(filter)
+
+    image_transform = dft2d(centered_nate, 1)
+    filter_transform = dft2d(centered_filter, 1)
+
+    filtered_image_freq = complex_hadamard(image_transform, filter_transform)
+
+    plot_transform_2d(image_transform)
+    plot_transform_2d(filter_transform)
+    plot_transform_2d(filtered_image_freq)
+
+    image_inverse_transform = dft2d(filtered_image_freq, -1)
+
+    uncentered = remove_centering_2d_transform(image_inverse_transform)
+
+    filtered_image = unpad(uncentered, pad_len)
+    plt.imshow(filtered_image, cmap="gray")
+    plt.show()
+
+    filtered_output = list_to_image(filtered_image)
+    filtered_output.save("nate-gauss.jpg")
+
+if apply_motion_blur:
+    nate = get_image_list("lenna.pgm")
+    noisy_nate = add_noise(nate, 0, 0)
+    centered_nate = center_2d_transform(noisy_nate)
+    image_transform = dft2d(centered_nate, 1)
+
+    filter = motion_blur_filter(1, .1, .1, 256)
+    filtered_image = complex_hadamard(image_transform, filter)
+
+    filtered_nate = dft2d(filtered_image, -1)
+
+    new_nate = remove_centering_2d_transform(filtered_nate)
+
+    nate_out = list_to_image(normalize_0_255(new_nate))
+    nate_out.save("lenna-motion.jpg")
+
+    noisy_nate_out = list_to_image(noisy_nate)
+    noisy_nate_out.save("lenna-noisy.jpg")
+
+if remove_motion_blur:
+    nate = get_image_list("lenna-motion.jpg")
+    centered_nate = center_2d_transform(nate)
+    image_transform = dft2d(centered_nate, 1)
+
+    filter = motion_blur_filter(1, .1, .1, 256)
+
+    #plot_transform_2d(filter)
+    #plot_transform_2d(complex_invert(filter, cutoff=900))
+
+    for cutoff in [10, 40, 70, 100]:
+        invert_filter = complex_invert(filter, cutoff=cutoff)
+
+        filtered_image = complex_hadamard(image_transform, invert_filter)
+
+        filtered_nate = dft2d(filtered_image, -1)
+
+        new_nate = remove_centering_2d_transform(filtered_nate)
+
+        nate_out = list_to_image(normalize_0_255(new_nate))
+        nate_out.save("lenna-restore-{}.jpg".format(cutoff))
+
+if remove_motion_blur_wiener:
+    nate = get_image_list("lenna-motion.jpg")
+    centered_nate = center_2d_transform(nate)
+    image_transform = dft2d(centered_nate, 1)
+
+    filter = motion_blur_filter(1, .1, .1, 256)
+    filter_square = add_zero(get_magnitude(filter, square=True))
+
+    for k in [.09, .07, .05, .03, .01, .001]:
+        noise_term = complex_division(filter_square,
+                                      add_scalar(filter_square, k), cutoff=500)
+        inverse_filtered = complex_division(image_transform, filter, cutoff=500)
+        filtered_image = complex_hadamard(noise_term, inverse_filtered)
+
+        filtered_nate = dft2d(filtered_image, -1)
+
+        new_nate = remove_centering_2d_transform(filtered_nate)
+
+        nate_out = list_to_image(normalize_0_255(new_nate))
+        nate_out.save("lenna-restore-wiener-{}.jpg".format(k))
